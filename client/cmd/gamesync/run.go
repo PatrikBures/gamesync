fbde7ed53b5b7e4501ddd607138a0023de5b6ee3
Patrik Bures <patrikbures5@gmail.com>
2025-12-11 19:41:19 +0100

    run command checks if pool exists

--- client/cmd/gamesync/run.go
+++ client/cmd/gamesync/run.go
@@ -12,14 +12,20 @@ var runCmd = &cobra.Command{
 	Short: "Sync saves and run game",
 	Args: cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		// check if game with that id exists
 		gameID := args[0]
 		game, err := config.GetGame(gameID)
 		if err != nil {
 			fmt.Printf("Error: %v\n", err)
 			return
 		}
-		fmt.Println("found game:", game.ID)
+
+		pool, err := config.GetPool(game.PoolID)
+		if err != nil {
+			fmt.Printf("Error: %v\n", err)
+			return
+		}
+
+		fmt.Printf("Syncing %s, in pool: %s\n", game.ID, pool.ID)
 	},
 }
 
