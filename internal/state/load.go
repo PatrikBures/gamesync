7c916f8d3198e16340d62a1ec8ef088aa991e60f
Patrik Bures <patrikbures5@gmail.com>
2026-01-15 00:06:37 +0100

    function for loading old state

---
+++ internal/state/load.go
@@ -0,0 +1,31 @@
+package state
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"gamesync/internal/config"
+	"os"
+	"path/filepath"
+)
+
+func GetOld(gameID string, verbose bool) (map[string]FileMeta, error) {
+	stateDir, err := config.GetStateDir()
+	if err != nil { return nil, err }
+
+	gameStateFile := filepath.Join(stateDir, gameID+".json")
+
+	if _, err := os.Stat(gameStateFile); errors.Is(err, os.ErrNotExist) {
+		if verbose { fmt.Println("old local state does not exist, loading empty state:", gameStateFile) }
+		return make(map[string]FileMeta), nil
+	}
+
+	stateBytes, err := os.ReadFile(gameStateFile)
+	if err != nil { return nil, err }
+
+	state := make(map[string]FileMeta)
+
+	json.Unmarshal(stateBytes, &state)
+
+	return state, nil
+}
