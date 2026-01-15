089bc7eea1e23ed57e48bba29f62cd40993162f9
Patrik Bures <patrikbures5@gmail.com>
2026-01-15 19:46:29 +0100

    The Syncing Logic
    function called HandleSync
    different modes:
    - auto
    - push
    - pull
    
    push and pull can be forced

--- cmd/gamesync/sync.go
+++ cmd/gamesync/sync.go
@@ -3,10 +3,8 @@ package main
 import (
 	"fmt"
 	"gamesync/internal/config"
-	"gamesync/internal/state"
 	"gamesync/internal/syncer"
 	"os"
-	"path/filepath"
 
 	"github.com/spf13/cobra"
 )
@@ -16,79 +14,8 @@ var syncCmd = &cobra.Command{
 	Short: "Either pushes or pulls save for GAME_ID",
 	Args: cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		game, _, err := config.GetGame(args[0])
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error getting game: %v\n", err)
-			os.Exit(40)
-		}
-
-		stateDir, err := config.GetStateDir()
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error getting state dir: %v\n", err)
-			os.Exit(41)
-		}
-
-		localState, err := state.Get(game.SavePath)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error getting local state of game with id \"%s\": %v\n", game.ID, err)
-			os.Exit(42)
-		}
-
-		oldLocalState, err := state.GetOld(game.ID, verbose)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error getting old local state for game with id \"%s\": %v\n", game.ID, err)
-			os.Exit(45)
-		}
-
-		remoteState, err := syncer.GetRemoteState(game.ID, config.Current.Server, verbose)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error getting remote state of game with id \"%s\": %v\n", game.ID, err)
-			os.Exit(44)
-		}
-
-		updateStateFile := false
-
-		compareResult, err := state.Compare(localState, oldLocalState, remoteState, false, verbose)
-
-		switch compareResult {
-		case state.SyncStateConflict:
-			fmt.Println("Conflict! Local and remote changes, aborting.")
-			os.Exit(100)
-		case state.SyncStateUnchanged:
-			fmt.Println("Already in sync, nothing to do.")
-		case state.SyncStatePush:
-			if err := syncer.SyncGame(*game, config.Current.Server, false, verbose); err != nil {
-				fmt.Fprintf(os.Stderr, "Error pushing game: %v\n", err)
-			}
-			fmt.Println("Local changes, pushing.")
-
-			updateStateFile = true
-		case state.SyncStatePull:
-			fmt.Println("Remote changes, pulling.")
-
-			if err := syncer.SyncGame(*game, config.Current.Server, true, verbose); err != nil {
-				fmt.Fprintf(os.Stderr, "Error pulling game: %v\n", err)
-			}
-
-			localState, err = state.Get(game.SavePath)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "Error getting local state of game with id \"%s\": %v\n", game.ID, err)
-				os.Exit(42)
-			}
-			updateStateFile = true
-		case state.SyncStateError:
-			fmt.Fprintf(os.Stderr, "Error comparing states: %v\n", err)
-		default:
-			panic(fmt.Errorf("unknown state from state.Compare(): %d", compareResult))
-		}
-
-		if updateStateFile {
-			stateFile := filepath.Join(stateDir, game.ID+".json")
-			if err := state.Write(localState, stateFile); err != nil {
-				fmt.Fprintf(os.Stderr, "Error writing state for game with id \"%s\":%v\n", game.ID, err)
-				os.Exit(43)
-			}
-			fmt.Println("Updated state file:", stateFile)
+		if err := syncer.HandleSync(config.Current.Server, args[0], syncer.ModeAuto, false,  verbose); err != nil {
+			fmt.Fprintf(os.Stderr, "Error syncing game: %v\n", err)
 		}
 	},
 }
--- internal/syncer/sync.go
+++ internal/syncer/sync.go
@@ -3,6 +3,7 @@ package syncer
 import (
 	"fmt"
 	"gamesync/internal/config"
+	"gamesync/internal/state"
 	"os"
 	"os/exec"
 	"path"
@@ -149,4 +150,119 @@ func ensureTrailingSep(p string) string {
 		return p + string(filepath.Separator)
 	}
 	return p
+}
+
+type SyncMode int
+const (
+	ModeAuto SyncMode = iota
+	ModePush
+	ModePull
+)
+
+func HandleSync(server config.ServerConfig, gameID string, mode SyncMode, force bool, verbose bool) error {
+	game, _, err := config.GetGame(gameID)
+	if err != nil {
+		return fmt.Errorf("Error getting game: %v\n", err)
+	}
+
+	stateDir, err := config.GetStateDir()
+	if err != nil {
+		return fmt.Errorf("Error getting state dir: %v\n", err)
+	}
+
+	localState, err := state.Get(game.SavePath)
+	if err != nil {
+		return fmt.Errorf("Error getting local state of game with id \"%s\": %v\n", game.ID, err)
+	}
+
+	oldLocalState, err := state.GetOld(game.ID, verbose)
+	if err != nil {
+		return fmt.Errorf("Error getting old local state for game with id \"%s\": %v\n", game.ID, err)
+	}
+
+	remoteState, err := GetRemoteState(game.ID, config.Current.Server, verbose)
+	if err != nil {
+		return fmt.Errorf("Error getting remote state of game with id \"%s\": %v\n", game.ID, err)
+	}
+
+	compareResult, err := state.Compare(localState, oldLocalState, remoteState, false, verbose)
+	if err != nil {
+		return fmt.Errorf("comparing states: %v\n", err)
+	}
+
+	var newStateToSave map[string]state.FileMeta = nil
+
+	switch compareResult {
+	case state.SyncStateUnchanged:
+		fmt.Println("Already in sync, nothing to do.")
+		return nil
+	case state.SyncStateError:
+		return fmt.Errorf("error during state comparison: %v\n", err)
+	}
+
+	switch mode {
+	case ModeAuto:
+		switch compareResult {
+		case state.SyncStateConflict:
+			return fmt.Errorf("conflict, remote and local changes")
+		case state.SyncStatePush:
+			fmt.Println("Pushing...")
+			if err := push(game, server, verbose); err != nil { return err }
+			newStateToSave = localState
+		case state.SyncStatePull:
+			fmt.Println("Pulling...")
+			if err := pull(game, server, verbose); err != nil { return err }
+			newStateToSave = remoteState
+		}
+	case ModePush:
+		switch compareResult {
+		case state.SyncStateConflict:
+			if !force {
+				return fmt.Errorf("conflict, remote and local changes")
+			}
+			fmt.Println("Force pushing over conflict...")
+		case state.SyncStatePush:
+			fmt.Println("Pushing...")
+		case state.SyncStatePull:
+			if !force {
+				return fmt.Errorf("aborted: unsynced remote changes")
+			}
+			fmt.Println("Force pushing, overwriting newer remote changes...")
+		}
+		if err := push(game, server, verbose); err != nil { return err }
+		newStateToSave = localState
+	case ModePull:
+		switch compareResult {
+		case state.SyncStateConflict:
+			if !force {
+				return fmt.Errorf("conflict, remote and local changes")
+			}
+			fmt.Println("Force pulling over conflict...")
+		case state.SyncStatePush:
+			if !force {
+				return fmt.Errorf("aborted: unsynced local changes")
+			}
+			fmt.Println("Force pulling, overwriting newer local changes...")
+		case state.SyncStatePull:
+			fmt.Println("Pulling...")
+		}
+		if err := pull(game, server, verbose); err != nil { return err }
+		newStateToSave = remoteState
+	}
+
+	stateFile := filepath.Join(stateDir, game.ID+".json")
+	if newStateToSave != nil {
+		if err := state.Write(newStateToSave, stateFile); err != nil {
+			return fmt.Errorf("writing state to file: %s: %v\n",stateFile, err)
+		}
+		fmt.Println("Updated state file:", stateFile)
+	}
+	return nil
+}
+
+func pull(game config.GameConfig, server config.ServerConfig, verbose bool) error {
+	return SyncGame(game, server, true, verbose)
+}
+func push(game config.GameConfig, server config.ServerConfig, verbose bool) error {
+	return SyncGame(game, server, false, verbose)
 }
