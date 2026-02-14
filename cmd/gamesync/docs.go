d7ee71fa06085a8e985f6c016db5829cfb4bfba0
Patrik Bures <patrikbures5@gmail.com>
2026-02-14 15:43:41 +0100

    gen doc cmd refactored

--- cmd/gamesync/docs.go
+++ cmd/gamesync/docs.go
@@ -9,33 +9,45 @@ import (
 	"github.com/spf13/cobra/doc"
 )
 
-var genDocCmd = &cobra.Command{
-	Use: "gen-man",
-	Short: "Generate man-pages for program",
-	Hidden: true,
-	// this prerun is here so that the root prerun does not run
-	PersistentPreRun: func(cmd *cobra.Command, args []string) {},
-	Run: func(cmd *cobra.Command, args []string) {
-		dir := "./manpages"
-		if _, err := os.Stat(dir); os.IsNotExist(err) {
-			ui.Error("Error dir does not exist: %s", dir)
-			os.Exit(1)
-		}
+type genDocCmd struct {
+	cmd *cobra.Command
+}
 
-		header := &doc.GenManHeader{
-			Title: "gamesync",
-			Section: "1",
-			Source: "Auto Generated",
-		}
+func newGenDocCmd() *genDocCmd {
+	root := genDocCmd{}
 
-		if err := doc.GenManTree(rootCmd, header, dir); err != nil {
-			ui.Error("Error generating man-pages: %v\n", err)
-			os.Exit(1)
-		}
-	},
+	cmd := &cobra.Command{
+		Use: "gen-man",
+		Short: "Generate man-pages for program",
+		Hidden: true,
+		// this prerun is here so that the root prerun does not run
+		PersistentPreRun: func(cmd *cobra.Command, args []string) {},
+		Run: func(cmd *cobra.Command, args []string) {
+			dir := "./manpages"
+			if _, err := os.Stat(dir); os.IsNotExist(err) {
+				ui.Error("Error dir does not exist: %s", dir)
+				os.Exit(1)
+			}
+
+			header := &doc.GenManHeader{
+				Title: "gamesync",
+				Section: "1",
+				Source: "Auto Generated",
+			}
+
+			if err := doc.GenManTree(rootCmd, header, dir); err != nil {
+				ui.Error("Error generating man-pages: %v\n", err)
+				os.Exit(1)
+			}
+		},
+	}
+
+	root.cmd = cmd
+
+	return &root
 }
 
 func init() {
-	rootCmd.AddCommand(genDocCmd)
+	rootCmd.AddCommand(newGenDocCmd().cmd)
 	rootCmd.DisableAutoGenTag = true
 }
