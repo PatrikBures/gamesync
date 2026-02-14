aa327966d7f8d548cec4f2f18a6fa461dcb746c2
Patrik Bures <patrikbures5@gmail.com>
2026-02-14 15:41:00 +0100

    sync cmd refactored

--- cmd/gamesync/sync.go
+++ cmd/gamesync/sync.go
@@ -7,18 +7,30 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var syncCmd = &cobra.Command{
-	Use: "sync GAME_ID",
-	Short: "Either pushes or pulls save for GAME_ID",
-	Args: cobra.ExactArgs(1),
-	Run: func(cmd *cobra.Command, args []string) {
-		gameID := args[0]
-		if err := syncer.HandleSync(current, gameID, syncer.ModeAuto, false); err != nil {
-			ui.Error("Error syncing game: %v\n", err)
-		}
-	},
+type syncCmd struct {
+	cmd *cobra.Command
+}
+
+func newSyncCmd() *syncCmd {
+	root := syncCmd{}
+
+	cmd := &cobra.Command{
+		Use: "sync GAME_ID",
+		Short: "Either pushes or pulls save for GAME_ID",
+		Args: cobra.ExactArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			gameID := args[0]
+			if err := syncer.HandleSync(current, gameID, syncer.ModeAuto, false); err != nil {
+				ui.Error("Error syncing game: %v\n", err)
+			}
+		},
+	}
+
+	root.cmd = cmd
+
+	return &root
 }
 
 func init() {
-	rootCmd.AddCommand(syncCmd)
+	rootCmd.AddCommand(newSyncCmd().cmd)
 }
