0a47b8f504bd4aed4c49e1f898772b55467172e4
Patrik Bures <patrikbures5@gmail.com>
2026-02-21 11:55:57 +0100

    sets ui level

--- cmd/gamesync/save_test.go
+++ cmd/gamesync/save_test.go
@@ -18,8 +18,12 @@ func TestSaveLs(t *testing.T) {
 	oldOut := ui.OutWriter
 	ui.OutWriter = &buf
 
+	oldLevel := ui.GetLevel()
+	ui.SetLevel(ui.LevelNormal)
+
 	t.Cleanup(func() {
 		ui.OutWriter = oldOut
+		ui.SetLevel(oldLevel)
 	})
 
 	cmd := newSaveLsCmd()
