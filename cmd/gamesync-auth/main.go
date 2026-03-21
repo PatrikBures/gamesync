cc11639848f7e192cb38d2bf4116859d3a402186
Patrik Bures <patrik@pabu.dev>
2026-03-21 10:31:17 +0100

    removed logging from auth

--- cmd/gamesync-auth/main.go
+++ cmd/gamesync-auth/main.go
@@ -19,38 +19,17 @@ func printKey() error {
 	}
 	fingerprint := os.Args[1]
 
-
-	// write fingerprint to tmp, for testing
-	f, err := os.CreateTemp("", "fingerprint_")
-	if err != nil {
-		log.Fatalln("create tmp file")
-	}
-	defer f.Close()
-	_, _ = fmt.Fprintln(f, "1")
-
-
 	db, err := dbm.OpenSQLite()
-	if err != nil { 
-		_, _ = fmt.Fprintln(f, err)
-		return err 
-	}
-	defer func() {
-		_, _ = fmt.Fprintln(f, err)
-		dbm.CloseDB(db, &err)
-	}()
-
+	if err != nil { return err }
+	defer dbm.CloseDB(db, &err)
 
 	keys, err := dbm.KeyGetKeysByFingerprint(db, fingerprint)
 	if err != nil {
-		_, _ = fmt.Fprintln(f, "3")
 		return fmt.Errorf("getting keys: %w", err)
 	}
 	for _, k := range keys {
 		fmt.Print(k)
-		_, _ = fmt.Fprint(f, k)
 	}
-
-
 
 	return nil
 }
