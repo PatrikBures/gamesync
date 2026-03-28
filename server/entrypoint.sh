7e1f230cce059e392fd2a9003fbf284ee3d0545d
Patrik Bures <patrik@pabu.dev>
2026-03-28 10:49:35 +0100

    returns if there is an error in entrypoint.sh

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -4,19 +4,19 @@ mkdir -p /etc/ssh/keys
 # creates host key if it does not exist
 if [ ! -f /etc/ssh/keys/ssh_host_ed25519_key ]; then
     echo "created host key"
-    ssh-keygen -A
+    ssh-keygen -A || return $?
     mv /etc/ssh/ssh_host_*key* /etc/ssh/keys/
 fi
 
 if ! id -u gamesync > /dev/null 2>&1; then
-    adduser -H -D -u 1000 -h /data/save -s /bin/sh gamesync
-    passwd -u gamesync
+    adduser -H -D -u 1000 -h /data/save -s /bin/sh gamesync || return $?
+    passwd -u gamesync || return $?
 fi
 
-gamesync-admin init dirs
-gamesync-admin init migrate
-gamesync-admin init perms
-gamesync-admin init roles
+gamesync-admin init dirs    || return 20
+gamesync-admin init migrate || return 21
+gamesync-admin init perms   || return 22
+gamesync-admin init roles   || return 23
 
 # exec replaces the shell with the process sshd so that pid 1 is sshd and docker can stop it cleanly
 # -D do not detach
