3ccee3ad22649677381ac04ca200dae8008a2488
Patrik Bures <patrik@pabu.dev>
2026-05-08 09:05:49 +0200

    removed all of the old code

--- cmd/gamesync/root.go
+++ cmd/gamesync/root.go
@@ -1,18 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"os"
-	"path/filepath"
-
-	"gamesync/internal/config"
-	"gamesync/internal/ui"
-	"gamesync/internal/vars"
 
 	"github.com/spf13/cobra"
 )
-
-var current config.Current
 
 type rootCmd struct {
 	cmd *cobra.Command
@@ -31,56 +23,10 @@ func newRootCmd() *rootCmd {
 	cmd := &cobra.Command{
 		Use: "gamesync",
 		Short: "Syncs save games to a server",
-		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
-			if current.ConfigPath == "" {
-				configEnv := os.Getenv("GAMESYNC_CONFIG")
-
-				if configEnv == "" {
-					configDir, err := config.GetConfigDir()
-					if err != nil {
-						return fmt.Errorf("error getting config dir: %w", err)
-					}
-					current.ConfigPath = filepath.Join(configDir, "config.yml")
-				} else {
-					current.ConfigPath = configEnv
-				}
-			} else {
-				var err error
-				current.ConfigPath, err = filepath.Abs(current.ConfigPath)
-				if err != nil {
-					return err
-				}
-			}
-			if err := config.Load(&current); err != nil {
-				return fmt.Errorf("loading config: %w", err)
-			}
-			if root.opts.debug {
-				ui.SetLevel(ui.LevelDebug)
-			} else if root.opts.verbose {
-				ui.SetLevel(ui.LevelVerbose)
-			}
-			
-			if root.opts.ignoreApiVersion {
-				vars.ApiVersion = "0"
-			}
-			return nil
-		},
 	}
-
-	cmd.PersistentFlags().StringVar(&current.ConfigPath, "config", "", "config file (default ~/.config/gamesync/config.yml)")
-	cmd.PersistentFlags().BoolVarP(&root.opts.verbose, "verbose", "v", false, "more verbose output")
-	cmd.PersistentFlags().BoolVarP(&root.opts.debug, "debug", "", false, "debug ouput")
-	cmd.PersistentFlags().BoolVarP(&root.opts.ignoreApiVersion, "ignore-api-version", "", false, "Do not check if remote has same api version. Use with caution!")
 
 	cmd.AddCommand(
 		newGenDocCmd().cmd,
-		newPullCmd().cmd,
-		newPushCmd().cmd,
-		newSyncCmd().cmd,
-		newSaveCmd().cmd,
-		newRemoteCmd().cmd,
-		newWrapCmd().cmd,
-		newVersionCmd().cmd,
 	)
 
 	cmd.DisableAutoGenTag = true
