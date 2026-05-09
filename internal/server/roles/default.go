9ee0ff8dc89c16f83cab3fe58d82c809a4dadb70
Patrik Bures <patrik@pabu.dev>
2026-05-09 09:40:09 +0200

    deletes role if it is disabled but exists

--- internal/server/roles/default.go
+++ internal/server/roles/default.go
@@ -17,19 +17,39 @@ func CreateDefaultRoles(q *query.Query, skipRoles []string) error {
 	roles := []*model.Role{
 		{ RoleID:  1, RoleName: "admin" },
 		{ RoleID: 50, RoleName: "standard" },
+		{ RoleID: 60, RoleName: "maintainer" },
 		{ RoleID: 99, RoleName: "none" },
 	}
 	for _, role := range roles {
+		existingRoleCount, err := q.Role.WithContext(ctx).Where(q.Role.RoleID.Eq(role.RoleID)).Count()
+		if err != nil {
+			return err
+		}
+
 		if slices.Contains(skipRoles, role.RoleName) {
+			if existingRoleCount > 0 {
+				info, err := q.Role.WithContext(ctx).Delete(role)
+				if err != nil {
+					if errors.Is(err, gorm.ErrForeignKeyViolated) {
+						slog.Error("could not delete role. there are probably users with that role preventing deletion.")
+						return err
+					}
+
+					return err
+				}
+				slog.Info("deleted", "role", *role, "info", info)
+				continue
+			}
 			slog.Info("skipped creating", "role", *role)
 			continue
 		}
 
+		if existingRoleCount > 0 {
+			slog.Info("already exists", "role", *role)
+			continue
+		}
+
 		if err := q.Role.WithContext(ctx).Create(role); err != nil {
-			if errors.Is(err, gorm.ErrDuplicatedKey) {
-				slog.Info("already exists", "role", *role)
-				continue
-			}
 			return err
 		}
 
