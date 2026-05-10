bfbaa470acaafda323df0bc197b3db53027a995d
Patrik Bures <patrik@pabu.dev>
2026-05-10 22:36:05 +0200

    perm and perms type

---
+++ internal/server/permissions/permissions.go
@@ -0,0 +1,15 @@
+package Permissions
+
+type Perms []Perm
+
+type Perm int
+const (
+	PermUsersDelete                Perm = 0
+	PermUsersUpdateName            Perm = 1
+	PermUsersUpdateNameOwn         Perm = 2
+	PermUsersUpdateRole            Perm = 3
+	PermRolesCreate                Perm = 4
+	PermRolesDelete                Perm = 5
+	PermRolesUpdateName            Perm = 6
+	PermRolesUpdatePerms           Perm = 7
+)
