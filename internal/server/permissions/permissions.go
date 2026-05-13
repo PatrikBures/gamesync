b268c3d537748a99bf938792b59e180b1fff5bb5
Patrik Bures <patrik@pabu.dev>
2026-05-13 16:47:46 +0200

    index starts at 1 cause postgres can't have that

--- internal/server/permissions/permissions.go
+++ internal/server/permissions/permissions.go
@@ -5,14 +5,14 @@ type Perms []Perm
 //go:generate go run golang.org/x/tools/cmd/stringer -type=Perm -trimprefix=Perm
 type Perm int
 const (
-	PermUsersDelete                Perm = 0
-	PermUsersUpdateName            Perm = 1
-	PermUsersUpdateNameOwn         Perm = 2
-	PermUsersUpdateRole            Perm = 3
-	PermRolesCreate                Perm = 4
-	PermRolesDelete                Perm = 5
-	PermRolesUpdateName            Perm = 6
-	PermRolesUpdatePerms           Perm = 7
+	PermUsersDelete                Perm = 1
+	PermUsersUpdateName            Perm = 2
+	PermUsersUpdateNameOwn         Perm = 3
+	PermUsersUpdateRole            Perm = 4
+	PermRolesCreate                Perm = 5
+	PermRolesDelete                Perm = 6
+	PermRolesUpdateName            Perm = 7
+	PermRolesUpdatePerms           Perm = 8
 )
 
 var AllPerms Perms = Perms{
