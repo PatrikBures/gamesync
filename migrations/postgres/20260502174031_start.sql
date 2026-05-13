7c3488c7e92bc134fa3014aa7ee7331c0b310c72
Patrik Bures <patrik@pabu.dev>
2026-05-13 16:55:13 +0200

    updated sql
    - primary key seperate from column
    - ensured postgres and sqlite are same

--- migrations/postgres/20260502174031_start.sql
+++ migrations/postgres/20260502174031_start.sql
@@ -19,35 +19,36 @@ CREATE TABLE users
     PRIMARY KEY (user_id),
     UNIQUE (user_id),
     UNIQUE (user_name),
-    UNIQUE (user_id, role_id),
-
     FOREIGN KEY (role_id) REFERENCES roles(role_id)
 );
 
 CREATE TABLE tokens
 (
-    token_id BIGSERIAL PRIMARY KEY,
+    token_id BIGSERIAL,
     user_id BIGINT NOT NULL,
     token_hash BYTEA NOT NULL,
 
+    PRIMARY KEY (token_id),
     UNIQUE (token_hash),
     FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
 CREATE TABLE permissions
 (
-    perm_id SERIAL PRIMARY KEY,
+    perm_id SERIAL,
     perm_name VARCHAR(50) NOT NULL,
 
+    PRIMARY KEY (perm_id),
     UNIQUE (perm_name)
 );
 
 CREATE TABLE role_permissions
 (
-    role_perm_id SERIAL PRIMARY KEY,
+    role_perm_id SERIAL,
     role_id INTEGER NOT NULL,
     perm_id INTEGER NOT NULL,
 
+    PRIMARY KEY (role_perm_id),
     UNIQUE (role_id, perm_id),
     FOREIGN KEY (role_id) REFERENCES roles(role_id) ON DELETE CASCADE,
     FOREIGN KEY (perm_id) REFERENCES permissions(perm_id) ON DELETE CASCADE
--- migrations/sqlite/20260502170122_start.sql
+++ migrations/sqlite/20260502170122_start.sql
@@ -2,52 +2,56 @@
 
 CREATE TABLE roles
 (
-    role_id INTEGER PRIMARY KEY,
+    role_id INTEGER,
     role_name TEXT NOT NULL,
 
+    PRIMARY KEY (role_id),
     UNIQUE (role_name)
 );
 
 CREATE TABLE users
 (
-    user_id INTEGER PRIMARY KEY,
+    user_id INTEGER,
     user_name TEXT NOT NULL,
     role_id INTEGER NOT NULL,
 
+    PRIMARY KEY (user_id),
+    UNIQUE (user_id),
     UNIQUE (user_name),
-    UNIQUE (user_id, role_id),
     FOREIGN KEY (role_id) REFERENCES roles(role_id)
 );
 
 CREATE TABLE tokens
 (
-    token_id INTEGER PRIMARY KEY,
-    user_id NOT NULL,
+    token_id INTEGER,
+    user_id INTEGER NOT NULL,
     token_hash BLOB NOT NULL,
 
+    PRIMARY KEY (token_id),
     UNIQUE (token_hash),
     FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
 CREATE TABLE permissions
 (
-    perm_id INTEGER PRIMARY KEY,
+    perm_id INTEGER,
     perm_name TEXT NOT NULL,
 
+    PRIMARY KEY (perm_id),
     UNIQUE (perm_name)
 );
 
 CREATE TABLE role_permissions
 (
-    role_perm_id INTEGER PRIMARY KEY,
+    role_perm_id INTEGER,
     role_id INTEGER NOT NULL,
     perm_id INTEGER NOT NULL,
 
+    PRIMARY KEY (role_perm_id),
     UNIQUE (role_id, perm_id),
     FOREIGN KEY (role_id) REFERENCES roles(role_id) ON DELETE CASCADE,
     FOREIGN KEY (perm_id) REFERENCES permissions(perm_id) ON DELETE CASCADE
 );
-
 
 -- +goose Down
 DELETE users;
