79de39bda842056a9f4c19432b5d315c07b94aac
Patrik Bures <patrik@pabu.dev>
2026-05-13 18:09:37 +0200

    can remove perms from role

--- internal/server/service/api_role_perms.go
+++ internal/server/service/api_role_perms.go
@@ -14,20 +14,41 @@ func (s *Service) GetRolePerms(ctx context.Context, params api.GetRolePermsParam
 }
 
 func (s *Service) PatchRolePerms(ctx context.Context, req api.OptPermDiff, params api.PatchRolePermsParams) (api.PatchRolePermsRes, error) {
-	add := make([]*model.RolePermission, 0, len(req.Value.Add))
-	for _, a := range req.Value.Add {
-		r := &model.RolePermission{
-			RoleID: params.RoleID,
-			PermID: int32(a),
+	if len(req.Value.Add) > 0 {
+		add := apiRoleToDbRole(req.Value.Add, params.RoleID)
+		if err := s.q.RolePermission.WithContext(ctx).Clauses(clause.OnConflict{DoNothing: true}).Create(add...); err != nil {
+			return &api.PatchRolePermsInternalServerError{}, ErrDatabase
 		}
-		add = append(add, r)
 	}
-	if err := s.q.RolePermission.WithContext(ctx).Clauses(clause.OnConflict{DoNothing: true}).Create(add...); err != nil {
-		return &api.PatchRolePermsInternalServerError{}, ErrDatabase
+	if len(req.Value.Remove) > 0 {
+		remove := make([]int32, 0, len(req.Value.Remove))
+		for _, r := range req.Value.Remove {
+			remove = append(remove, int32(r))
+		}
+		_, err := s.q.RolePermission.WithContext(ctx).
+			Where(
+				s.q.RolePermission.RoleID.Eq(params.RoleID),
+				s.q.RolePermission.PermID.In(remove...),
+			).Delete()
+		if err != nil {
+			return &api.PatchRolePermsInternalServerError{}, ErrDatabase
+		}
 	}
 	return &api.PatchRolePermsCreated{}, nil
 }
 
 func (s *Service) PutRolePerms(ctx context.Context, req api.PermArray, params api.PutRolePermsParams) (api.PutRolePermsRes, error) {
 	return nil, nil
+}
+
+func apiRoleToDbRole(perms api.PermArray, roleID int32) []*model.RolePermission {
+	newPerms := make([]*model.RolePermission, 0, len(perms))
+	for _, a := range perms {
+		r := &model.RolePermission{
+			RoleID: roleID,
+			PermID: int32(a),
+		}
+		newPerms = append(newPerms, r)
+	}
+	return newPerms
 }
