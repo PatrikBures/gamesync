310e2aa11b250d54bb5ce94805d927e465162bbd
Patrik Bures <patrik@pabu.dev>
2026-05-17 18:18:26 +0200

    moved create user to seperate functions so it can be used elsewhere

--- internal/server/service/api_users.go
+++ internal/server/service/api_users.go
@@ -5,9 +5,9 @@ import (
 	"crypto/sha256"
 	"encoding/base64"
 	"errors"
-	"fmt"
 	"gamesync/internal/model"
 	api "gamesync/internal/ogen"
+	"gamesync/internal/query"
 	"gamesync/internal/server"
 	"gamesync/internal/server/permissions"
 	"log/slog"
@@ -57,45 +57,22 @@ func (s *Service) PostUsers(ctx context.Context, req api.OptUserName) (result ap
 	if !req.Set {
 		return &api.PostUsersNotAcceptable{}, server.ErrMissingBody
 	}
-	user := model.User{
+	user := &model.User{
 		UserName: req.Value.UserName,
 		RoleID: s.o.DefaultRoleID,
 	}
 	tx := s.q.Begin()
-	defer func() {
-		if recover() != nil || err != nil {
-			if e := tx.Rollback(); e != nil {
-				slog.Error("failed rollback", "error", e)
-			}
-		}
-	}()
 
-	if err = tx.User.WithContext(ctx).Create(&user); err != nil {
-		if errors.Is(err, gorm.ErrDuplicatedKey) {
-			return &api.PostUsersConflict{}, server.ErrDuplicateKey
-		} else if errors.Is(err, gorm.ErrForeignKeyViolated) {
-			return &api.PostUsersInternalServerError{}, fmt.Errorf("invalid role")
-		}
-		return &api.PostUsersInternalServerError{}, server.ErrDatabase
-	}
-
-	token, err := generateToken()
+	token64, err := createUser(tx, ctx, user)
 	if err != nil {
-		return &api.PostUsersInternalServerError{}, server.ErrToken
-	}
-	token64 := base64.URLEncoding.EncodeToString(token)
-	tokenHash := sha256.Sum256(token)
-
-	t := model.Token{
-		UserID: user.UserID,
-		TokenHash: tokenHash[:],
-	}
-	if err = tx.Token.WithContext(ctx).Create(&t); err != nil {
-		return &api.PostUsersInternalServerError{}, server.ErrDatabase
-	}
-
-	if err = tx.Commit(); err != nil {
-		return &api.PostUsersInternalServerError{}, server.ErrDatabase
+		switch err {
+		case server.ErrDatabase:
+			return &api.PostUsersInternalServerError{}, err
+		case server.ErrDuplicateKey:
+			return &api.PostUsersConflict{}, err
+		case server.ErrToken:
+			return &api.PostUsersInternalServerError{}, err
+		}
 	}
 
 	return &api.UserNewReturn{
@@ -104,4 +81,46 @@ func (s *Service) PostUsers(ctx context.Context, req api.OptUserName) (result ap
 		RoleID: user.RoleID,
 		Token: token64,
 	}, nil
+}
+
+
+
+func createUser(tx *query.QueryTx, ctx context.Context, user *model.User) (token64 string, err error) {
+	defer func() {
+		if recover() != nil || err != nil {
+			if e := tx.Rollback(); e != nil {
+				slog.Error("failed rollback", "error", e)
+			}
+		}
+	}()
+
+	if err = tx.User.WithContext(ctx).Create(user); err != nil {
+		if errors.Is(err, gorm.ErrDuplicatedKey) {
+			return "", server.ErrDuplicateKey
+		} else if errors.Is(err, gorm.ErrForeignKeyViolated) {
+			return "", server.ErrDatabase
+		}
+		return "", server.ErrDatabase
+	}
+
+	token, err := generateToken()
+	if err != nil {
+		return "", server.ErrToken
+	}
+	token64 = base64.URLEncoding.EncodeToString(token)
+	tokenHash := sha256.Sum256(token)
+
+	t := model.Token{
+		UserID: user.UserID,
+		TokenHash: tokenHash[:],
+	}
+	if err = tx.Token.WithContext(ctx).Create(&t); err != nil {
+		return "", server.ErrDatabase
+	}
+
+	if err = tx.Commit(); err != nil {
+		return "", server.ErrDatabase
+	}
+
+	return token64, err
 }
