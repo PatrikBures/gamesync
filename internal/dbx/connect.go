46cacebb2938dbe69b649236584c009db71fa521
Patrik Bures <patrik@pabu.dev>
2026-05-20 08:59:15 +0200

    custom logger which silences not found errors

--- internal/dbx/connect.go
+++ internal/dbx/connect.go
@@ -2,19 +2,35 @@ package dbx
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"time"
 
 	"github.com/libtnb/sqlite"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 func ConnectDb(dbType string, dsn string) (*gorm.DB, error) {
 	var err error
 	var db *gorm.DB
 
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags),
+		logger.Config{
+			SlowThreshold: time.Second,
+			LogLevel: logger.Error,
+			IgnoreRecordNotFoundError: true,
+		},
+	)
+
 	switch dbType{
 	case "sqlite":
-		db, err = gorm.Open(sqlite.Open(dsn), &gorm.Config{TranslateError: true})
+		db, err = gorm.Open(sqlite.Open(dsn), &gorm.Config{
+			TranslateError: true, 
+			Logger: newLogger,
+		})
 	case "postgres":
 		db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{TranslateError: true})
 	default:
