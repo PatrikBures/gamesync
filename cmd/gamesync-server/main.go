cf3dcfc761375079ed15e8ac556ddc54717e1fe7
Patrik Bures <patrik@pabu.dev>
2026-05-23 18:55:39 +0200

    request logger -> request logs

--- cmd/gamesync-server/main.go
+++ cmd/gamesync-server/main.go
@@ -37,7 +37,7 @@ func start() error {
 	serverConfig.AddStringVar(&c.dbUrl, "db-url", server.DefaultSQLitePath, "Url to postgres db or path to SQLite db-file")
 	serverConfig.AddStringVar(&c.disabledRoles, "disabled-roles", "", "Default roles to disable, seperated by '|'")
 	serverConfig.AddIntVar(&c.defaultRoleID, "default-role-id", 50, "Default role id for newly created users. Role needs to exist for creation of new users to work")
-	serverConfig.AddBoolVar(&c.requestLogger, "request-logger", false, "Enable logs for requests")
+	serverConfig.AddBoolVar(&c.requestLogger, "request-logs", false, "Enable logs for requests")
 
 	flag.Parse()
 
