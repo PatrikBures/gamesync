2a8d28ae8f527a5cadb5ebcafce24e0addf426be
Patrik Bures <patrik@pabu.dev>
2026-05-29 07:43:07 +0200

    pprof and trace

--- cmd/gamesync/main.go
+++ cmd/gamesync/main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"os"
+	"runtime/pprof"
+	"runtime/trace"
+)
+
 func main() {
+	// these debug parts should be removed or made into flags in the future
+	fcpu, _ := os.Create("cpu.prof")
+	pprof.StartCPUProfile(fcpu)
+	defer pprof.StopCPUProfile()
+
+	ftrace, _ := os.Create("trace.out")
+	trace.Start(ftrace)
+	defer trace.Stop()
+
 	Execute()
 }
